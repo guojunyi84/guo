3e2d079e06b5920e8351da21b992b6c49b4dff24
guojunyi84 <1021545981@qq.com>
2019-10-24 16:59:10 +0800

    'asd'

--- nannv/src/component/admin/index.js
+++ nannv/src/component/admin/index.js
@@ -1,14 +1,14 @@
 import React,{Component} from 'react'
-import { Menu, Icon } from 'antd';
+import { Menu, Icon,Button,Dropdown} from 'antd';
 import './index.less'
 let navData=[
   {name:'首页',path:'/home'},
   {name:'音乐模块',path:'/home'},
-  {name:'工具模块',
+  {name:'管理',
     path:'/user',
     children:[
-      {name:'小应用',path:'/user/list'},
-      {name:'富文本编辑器',path:'/user/del'},
+      {name:'用户查询管理',path:'/user/list'},
+      {name:'音乐点击量',path:'/user/del'},
       {name:'待办事项',path:'/user/del'}
     ]
   },
@@ -22,9 +22,39 @@ let navData=[
   {name:'搜索模块',path:'/home'},
 ]
 const { SubMenu } = Menu;
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+        个人信息
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+        联系服务
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://10.60.14.197:3000/#/login">
+        退出登录
+      </a>
+    </Menu.Item>
+  </Menu>
+);
 
 
 class Admin extends Component{
+  state = {
+    collapsed: false,
+  };
+  
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+  
+  
   renderItem=(data)=>{
     return data.map((item,index)=>{
       if(item.children){
@@ -38,25 +68,56 @@ class Admin extends Component{
         return <Menu.Item>{item.name}</Menu.Item>
       }
     })
-    
   }
+  
   render(){
     return(
       <div className='admin'>
         <div className='admin-nav'>
+          <div style={{ width: 256 }}>
+            <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+              <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+            </Button>
+            <Menu
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              mode="inline"
+              theme="dark"
+              inlineCollapsed={this.state.collapsed}
+            >
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="appstore" />
+                    <span>Navigation Two</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+                <SubMenu key="sub3" title="Submenu">
+                  <Menu.Item key="11">Option 11</Menu.Item>
+                  <Menu.Item key="12">Option 12</Menu.Item>
+                </SubMenu>
+              </SubMenu>
+            </Menu>
+          </div>
           <Menu mode="inline" style={{ width: 256 }}>,
             {this.renderItem(navData)}
           </Menu>
         </div>
         <div className='admin-content'>
-          <div>
-            头部信息
+          <div className='admin-content-tou'>
+            <Dropdown className='admin-content-tou-user' overlay={menu} placement="bottomRight">
+              <Button>个人中心</Button>
+            </Dropdown>
           </div>
           <div>
             {this.props.children}
           </div>
-          <div>
-            底部信息
+          <div className='admin-content-bottom'>
+            React-Admin ©2019 Created by 865470087@qq.com
           </div>
         </div>
       </div>
